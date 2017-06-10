62f9eae3ccb6db5fa9b596ad07ab25c827debd11
soukwinder <hallosir2>
2017-06-11 01:03:02 +0200

    visualistatie 2

--- src/app/pages/charts/components/chartistJs/chartistJs.component.ts
+++ src/app/pages/charts/components/chartistJs/chartistJs.component.ts
@@ -12,7 +12,6 @@ import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 export class ChartistJs implements OnInit{
   sensorType = ['temperatuur', 'luchtvochtigheid', 'licht'];
- // months = ['Januari','Februari','Maart','April','Mei','Juni','Juli','Augustus','September','Oktober','November','December'];
   months = [1,2,3,4,5,6,7,8,9,11,12];
 
   years = ['2016','2017','2018']
@@ -24,12 +23,22 @@ export class ChartistJs implements OnInit{
 
 
   lineData;
+  lineDataa;
+  lineData1;
+  lineData1a;
+
+  number;
 
   data: any;
+  data1: any;
+  data2: any;
   labelArray = [];
   meanArray = [];
+  meanArray1 = [];
   productsAfterChangeEvent = [];
   productForm : FormGroup;
+  productsAfterChangeEvent1 = [];
+  productForm1 : FormGroup;
 
   constructor(private dataService: PieChartService, fb: FormBuilder) {
     this.productForm = fb.group({
@@ -37,18 +46,18 @@ export class ChartistJs implements OnInit{
       years: [],
       productType: [],
       product: [],
+      product1: [],
     });
-    this.data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          borderColor: '#F53636'
-        }
-      ]
-    }
+    this.productForm1 = fb.group({
+      months1: [],
+      years1: [],
+      productType1: [],
+      product: [],
+      product1: [],
+    });
+
+    this.number = 1;
+
   }
 
   ngOnInit(){
@@ -60,9 +69,10 @@ export class ChartistJs implements OnInit{
   test(){
     const productType = this.productForm.get('productType').value;
     this.productsAfterChangeEvent = this.allProducts.filter(p => p.type == productType);
-    const months = this.productForm.get('months').value;
-    const years = this.productForm.get('years').value;
-    console.log(this.productForm.get('years').value)
+  }
+  test1(){
+    const productType = this.productForm1.get('productType1').value;
+    this.productsAfterChangeEvent1 = this.allProducts.filter(p => p.type == productType);
   }
 
   getTotalIdsTemp(){
@@ -106,40 +116,111 @@ export class ChartistJs implements OnInit{
     const months = this.productForm.get('months').value;
     const years = this.productForm.get('years').value;
     const id = this.productForm.get('product').value;
+    const id1 = this.productForm.get('product1').value;
 
     this.dataService.getLineData(productType, years, months, id)
       .subscribe(
         (data => {
           this.lineData = data.json()
-          this.createLineData(this.lineData)
+          this.createLineData(this.lineData, id, id1)
         })
       )
-    console.log(productType+", "+months+", "+years+", "+id)
+    this.dataService.getLineData(productType, years, months, id1)
+      .subscribe(
+        (data => {
+          this.lineDataa = data.json()
+        }))
   }
 
-  createLineData(linedata){
+  update1(event: Event){
+    const productType = this.productForm1.get('productType1').value;
+    const months = this.productForm1.get('months1').value;
+    const years = this.productForm1.get('years1').value;
+    const id = this.productForm1.get('product').value;
+    const id1 = this.productForm1.get('product1').value;
 
+
+    this.dataService.getLineData(productType, years, months, id)
+      .subscribe(
+        (data => {
+          this.lineData1 = data.json()
+          this.createLineData1(this.lineData1, id, id1)
+        })
+      )
+      this.dataService.getLineData(productType, years, months, id1)
+        .subscribe(
+          (data => {
+            this.lineData1a = data.json()
+          })
+        )
+  }
+
+  createLineData(linedata, id, id1){
+
+    console.log(id1)
     this.meanArray = []
+    this.meanArray1 = []
     this.labelArray = []
 
-    for(var i = 1; i < linedata.length; i++){
-
+    for(var i = 0; i < linedata.length; i++){
       this.meanArray.push(JSON.parse(JSON.stringify(linedata))[i].mean)
-      this.labelArray.push(i)
+      if(id1 == 'Selecteer:' || id1 == null){
+        console.log('geen mean')
+      } else{
+        this.meanArray1.push(JSON.parse(JSON.stringify(this.lineDataa))[i].mean)
+      }
+      this.labelArray.push(i+1)
     }
-
-    console.log(this.meanArray+", "+this.labelArray)
-
     this.data = {
       labels: this.labelArray,
       datasets: [
         {
-          label: 'First Dataset',
+          label: 'ID: '+id,
           data: this.meanArray,
           fill: false,
           borderColor: '#F53636'
+        },
+        {
+          label: 'ID :'+id1,
+          data: this.meanArray1,
+          fill: false,
+          borderColor: '#F5f500'
         }
       ]
     }
   }
+
+  createLineData1(linedata, id, id1){
+    this.meanArray = []
+    this.meanArray1 = []
+    this.labelArray = []
+
+    for(var i = 0; i < linedata.length; i++){
+      this.meanArray.push(JSON.parse(JSON.stringify(linedata))[i].mean)
+      if(id1 == 'Selecteer:' || id1 == null){
+        console.log('geen mean')
+      } else{
+        this.meanArray1.push(JSON.parse(JSON.stringify(this.lineData1a))[i].mean)
+      }
+      this.labelArray.push(i+1)
+    }
+
+    this.data1 = {
+      labels: this.labelArray,
+      datasets: [
+        {
+          label: 'ID: '+id,
+          data: this.meanArray,
+          fill: false,
+          borderColor: '#F53636'
+        },
+        {
+          label: 'ID :'+id1,
+          data: this.meanArray1,
+          fill: false,
+          borderColor: '#F5f500'
+        }
+      ]
+    }
+}
 }
